64e01dd2d338d10ade13ee3ea3059ab98de74635
tshifhiwa <sinugotshifhiwa4@gmail.com>
2023-12-11 15:46:37 +0200

    commit on web actions class, adding methods for fluent waits

--- src/test/java/webAutomation/webUtilities/WebActions.java
+++ src/test/java/webAutomation/webUtilities/WebActions.java
@@ -1,6 +1,82 @@
 package webAutomation.webUtilities;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.Wait;
+
+import java.time.Duration;
+
 public class WebActions {
 
+
+    public void clickObjects(WebElement element, WebDriver driver){
+
+        try{
+
+            Wait<WebDriver> wait = new FluentWait<>(driver)
+                    .withTimeout(Duration.ofSeconds(5000))
+                    .pollingEvery(Duration.ofMillis(1000))
+                    .ignoring(WebDriverException.class);
+            wait.until(ExpectedConditions.elementToBeClickable(element));
+            element.click();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage() + " " + "Not Found");
+        }
+    }
+
+    public void passData(WebElement element, WebDriver driver, String passValue){
+
+        try{
+
+            Wait<WebDriver> wait = new FluentWait<>(driver)
+                    .withTimeout(Duration.ofSeconds(5000))
+                    .pollingEvery(Duration.ofMillis(1000))
+                    .ignoring(WebDriverException.class);
+            wait.until(ExpectedConditions.elementToBeClickable(element));
+            element.clear();
+            element.sendKeys(passValue);
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage() + " " + "Not Found");
+        }
+    }
+
+    public void selectObjects(WebElement element, WebDriver driver, String selectMethod, Object vValue){
+
+        Select select = new Select(element);
+
+        try{
+
+            Wait<WebDriver> wait = new FluentWait<>(driver)
+                    .withTimeout(Duration.ofSeconds(5000))
+                    .pollingEvery(Duration.ofMillis(1000))
+                    .ignoring(WebDriverException.class);
+            wait.until(ExpectedConditions.elementToBeClickable(element));
+
+            switch (selectMethod.toUpperCase()){
+
+                case "SELECTBYINDEX":
+                    select.selectByIndex((Integer) vValue);
+                    break;
+
+                case "SELECTBYVALUE":
+                    select.selectByValue((String) vValue);
+                    break;
+
+                case "SELECTBYVISIBLETEXT":
+                    select.selectByVisibleText((String) vValue);
+                    break;
+            }
+
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage() + " " + "Not Found");
+        }
+    }
 
 }
